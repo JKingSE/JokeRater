bbf29f24312976b02673453e2dcc35df3b94a5f0
Norwal <jfk1307@g.rit.edu>
2017-09-27 15:05:02 -0400

    Added some architecture to jokebean and started the setRatings method.  This includes a basic algorithm to calculate overall rating

--- src/main/java/Beans/JokeBean.java
+++ src/main/java/Beans/JokeBean.java
@@ -4,11 +4,23 @@
  * and open the template in the editor.
  */
 package Beans;
-
+import Models.Joke;
 /**
  *
  * @author John King
  */
 public class JokeBean {
+    public Joke joke;
     
+    public JokeBean(){
+        
+    }
+    
+    public void setRatings(int funniness, int punniness, int edginess){
+        joke.setFunniness(funniness);
+        joke.setPunniness(punniness);
+        joke.setEdginess(edginess);
+        int overallRating = Math.round(funniness+punniness+edginess)/3;
+        joke.setOverallRating(overallRating);
+    }
 }
