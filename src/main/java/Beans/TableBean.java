aa80e90a55dc45e1e5ddb530dd4c1d6b0bd9d34b
Michael <Michael@Michael-laptop.wireless.rit.edu>
2017-10-26 16:35:46 -0400

    more database linkup, dialog & row select is WIP

--- src/main/java/Beans/TableBean.java
+++ src/main/java/Beans/TableBean.java
@@ -17,6 +17,7 @@ import org.primefaces.event.SelectEvent;
 public class TableBean implements Serializable{
     
     private List<Joke> jokes;
+    private Joke selection;
     
     @ManagedProperty(value = "#{jokeDatabase}")
     private JokeDatabase jokeDatabase;
@@ -35,6 +36,14 @@ public class TableBean implements Serializable{
         this.jokes = jokes;
     }
 
+    public Joke getSelection() {
+        return selection;
+    }
+
+    public void setSelection(Joke selection) {
+        this.selection = selection;
+    }
+    
     public JokeDatabase getJokeDatabase() {
         return jokeDatabase;
     }
@@ -45,27 +54,9 @@ public class TableBean implements Serializable{
     
     
     
-//    private List<Joke> allJokes;
-//    private Joke selection;
+
 //    private List<Joke> filtered;
 
-    //***Getters & Setters***//
-//    public List<Joke> getAllJokes() {
-//        return allJokes;
-//    }
-//
-//    public void setAllJokes(List<Joke> allJokes) {
-//        this.allJokes = allJokes;
-//    }
-//    
-//    public Joke getSelection() {
-//        return selection;
-//    }
-//
-//    public void setSelection(Joke selection) {
-//        this.selection = selection;
-//    }
-//    
 //    public List<Joke> getFiltered() {
 //        return filtered;
 //    }
@@ -100,25 +91,5 @@ public class TableBean implements Serializable{
 //        }
 //    }
     
-//    public void dummyvalues() {
-//       DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-//       CharSequence cseq = "1989-10-10 10:10:10";
-//       
-//       List<Joke> dummylist = new ArrayList();
-//       for(int i=1; i<10; i++) {
-//           Joke joke = new Joke();
-//           joke.setId(i);
-//           joke.setJoke("test");
-//           joke.setContext("test");
-//           joke.setEdginess(5);
-//           joke.setPunniness(5);
-//           joke.setFunniness(5);
-//           joke.setDateAdded(ZonedDateTime.parse(cseq, dtf));
-//           dummylist.add(joke);
-//       }
-//       
-//       this.allJokes = dummylist;
-//       this.filtered = dummylist;
-//    }
        
 } // TableBean.java
